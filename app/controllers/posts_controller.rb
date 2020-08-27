4d59ff76fb60692a0b7d1f4f37398f2fad445cff
kiptubei <kiptubei@gmail.com>
2020-08-27 11:47:49 +0300

    Made gemfiles consistent ,user can see only friends posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,15 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    # @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+
+    # @timeline = Post.all.ordered_by_most_recent.includes(current_user)
+
+    if current_user.friend?(:user)
+      @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    else
+      @timeline_posts = []
+    end
   end
 
   def post_params
